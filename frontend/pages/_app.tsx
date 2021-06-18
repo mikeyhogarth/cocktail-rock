88e7d0355261c0a5c5e3684ebf2d4a616dc25b0b
Mike Hogarth <mikehogarth20@hotmail.com>
2021-06-18 19:36:24 +0100

    Add tailwind

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,13 +4,15 @@ import type { AppProps } from "next/app";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header>
+      <header className="text-3xl text-center bg-black text-white p-5">
         <h1>Cocktail Rock</h1>
       </header>
-      <main>
+      <main className="px-40 text-center">
         <Component {...pageProps} />
       </main>
-      <footer>Cocktail Rock &copy;{new Date().getFullYear()}</footer>
+      <footer className="text-center p-5">
+        Cocktail Rock &copy;{new Date().getFullYear()}
+      </footer>
     </>
   );
 }
